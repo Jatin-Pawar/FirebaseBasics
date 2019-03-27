6bad0305d60ff03c450632e572d6f3c5d234b1cb
Yojit-Kaushal <yojitkaushal@hotmail.com>
2019-03-27 18:13:27 +0530

    Checkpoint 1

--- app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.firebaseapp;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText myEditText;
+    EditText myEditText, myKeyValue;
     Button myApplyBtn;
-    String myStringData;
+    String myStringData, myKeyValueData;
 
     Firebase myFirebase;
 
@@ -23,11 +26,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myEditText = (EditText) findViewById(R.id.editText);
+        myKeyValue = (EditText) findViewById(R.id.editText2);
+
         myApplyBtn = (Button) findViewById(R.id.button);
 
-        myStringData = myEditText.getText().toString();
+
 
         Firebase.setAndroidContext(this);
-        myFirebase = new Firebase("https://fir-app-13aff.firebaseio.com");
+
+        String DeviceId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        myFirebase = new Firebase("https://fir-app-13aff.firebaseio.com/Users" + DeviceId);
+
+        myApplyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                myStringData = myEditText.getText().toString();
+                myKeyValueData = myKeyValue.getText().toString();
+                Firebase myNewChild = myFirebase.child(myKeyValueData);
+                myNewChild.setValue(myStringData);
+
+                Toast.makeText(MainActivity.this, myKeyValueData +" is updated with "+ myStringData ,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
